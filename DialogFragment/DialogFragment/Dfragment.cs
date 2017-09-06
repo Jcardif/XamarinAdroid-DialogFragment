24ae66786014167ea8305ec8afb69774e59685ac
Jcardif <josh.cardif14@outlook.com>
2017-09-06 05:48:26 +0300

    Complete dialog fragment

---
+++ DialogFragment/DialogFragment/Dfragment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Test;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace DialogFragment
+{
+    public class Dfragment : Android.App.DialogFragment
+    {
+        private ListView lstView;
+        private Button okbtButton;
+
+        private string[] planes =
+        {
+            "Boeing 7x7 series", "Boeing 747", "Boeing 707", "Boeing 717", "Boeing 727", "Boeing 737", "Boeing 757"
+        };
+
+        private ArrayAdapter arrayAdapter;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+
+            View v = inflater.Inflate(Resource.Layout.Fragment, container, false);
+
+            this.Dialog.SetTitle("Boeing Series");
+
+            lstView = v.FindViewById<ListView>(Resource.Id.lview);
+            arrayAdapter=new ArrayAdapter(this.Activity, Android.Resource.Layout.SimpleListItem1,planes);
+
+            lstView.Adapter = arrayAdapter;
+            okbtButton = v.FindViewById<Button>(Resource.Id.Ok);
+            okbtButton.Click += delegate
+            {
+                //stuff
+                Dismiss();
+            };
+            lstView.ItemClick += LstView_ItemClick;
+                return v;
+        }
+
+        private void LstView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Toast.MakeText(this.Activity, planes[e.Position]+"Clicked", ToastLength.Short).Show();
+        }
+    }
+}
---
+++ DialogFragment/DialogFragment/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace DialogFragment
+{
+    [Activity(Label = "DialogFragment", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private FragmentManager fm;
+        private Dfragment planes;
+        private Button show;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+            show = FindViewById<Button>(Resource.Id.btnShow);
+            show.Click += Show_Click;
+
+            fm = this.FragmentManager;
+            planes=new Dfragment();
+        }
+
+        private void Show_Click(object sender, System.EventArgs e)
+        {
+            planes.Show(fm, "plane_TAG");
+        }
+    }
+}
+
